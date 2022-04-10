6f804306e1df9f0033801219fdeede82fed59b94
SimranThakur07 <99080528+SimranThakur07@users.noreply.github.com>
2022-04-10 16:02:12 +0530

    Completed

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ form.addEventListener('submit', (e) => {
 			input.parentElement.classList.add('error');
 		} else {
 			input.parentElement.classList.remove('error');
-			// if (input.type == 'email') {
-			// 	if (validateEmail(input.value)) {
-			// 		input.parentElement.classList.remove('error');
-			// 	} else {
-			// 		input.parentElement.classList.add('error');
-			// 	}
-			// }
+			if (input.type == 'email') {
+				if (validateEmail(input.value)) {
+					input.parentElement.classList.remove('error');
+				} else {
+					input.parentElement.classList.add('error');
+				}
+			}
 		}
 	});
  });
 
-// function validateEmail (email) {
-// 	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-// 	return re.test(String(email).toLowerCase());
-// }
+function validateEmail (email) {
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
